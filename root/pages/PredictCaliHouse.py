a6afa5a51a55050c16291764d424a0615a310c48
Hunglee-wjbu <dungvanle747@gmail.com>
2022-12-10 22:00:09 +0700

    fix direct

--- root/pages/PredictCaliHouse.py
+++ root/pages/PredictCaliHouse.py
@@ -8,7 +8,7 @@ from sklearn.impute import SimpleImputer
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error
-from pages.Util import CombinedAttributesAdder
+from root.pages.Util import CombinedAttributesAdder
 from sklearn.model_selection import cross_val_score
 
 import streamlit as st
